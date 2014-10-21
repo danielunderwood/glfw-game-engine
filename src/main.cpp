43390e60892a45e52e66759ee9e1e26d046c58a2
Daniel Underwood <daniel.underwood13@gmail.com>
2014-10-20 20:18:41 -0400

    Fix issue with empty while loop exiting

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char ** argv)
     // Open Window
     Application * app = new Application();
     app->addWindow(320, 620, true, "Test", true);
-    // TODO: Figure out what is happening here
-    while(app->mainWindow->renderFrame()) {}
+    while(app->mainWindow->renderFrame());
     return 0;
 }
