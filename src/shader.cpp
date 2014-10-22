a0563121f2a252f2573bf82fe87be2bbb57539d3
Daniel Underwood <daniel.underwood13@gmail.com>
2014-10-21 21:48:54 -0400

    Add error handling for shader class if file doesn't exist or if shader fails to compile

--- src/shader.cpp
+++ src/shader.cpp
@@ -15,6 +15,19 @@ Shader::Shader(const char * filename, GLenum shaderType) :
     // TODO: Handle the case of file not existing
     std::ifstream shaderFileStream(filename);
 
+    // Exit if file doesn't exist
+    if(!shaderFileStream)
+    {
+        fprintf(stderr, "Could not open shader '%s': File Does Not Exist", filename);
+
+        // Set shader ID to 0 to stop problems from occurring
+        shaderID = 0;
+
+        // Prematurely return from the constructor
+        // TODO: Find a better way of doing this
+        return;
+    }
+
     while(std::getline(shaderFileStream, line))
     {
         shaderString += line;
@@ -35,6 +48,27 @@ Shader::Shader(const char * filename, GLenum shaderType) :
 
     // Compile
     glCompileShader(shaderID);
+
+    // Check for error in compiling shader
+    GLint compileStatus;
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &compileStatus);
+
+    if(compileStatus != GL_TRUE)
+    {
+        // Get length of compiler log
+        GLint compileLogLength;
+        glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &compileLogLength);
+
+        // Get compile log
+        GLchar * compileLog = new GLchar[compileLogLength + 1]; // Add 1 for null terminator
+        glGetShaderInfoLog(shaderID, compileLogLength + 1, NULL, compileLog);
+
+        // Print status
+        fprintf(stderr, "Shader '%s' failed to compile:\n%s\n", filename, compileLog);
+
+        // TODO: Figure out what to do when a shader fails to compile. It will currently just result
+        // in graphical bugs
+    }
 }
 
 Shader::~Shader()
