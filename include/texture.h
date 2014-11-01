1b97be7afd5484f00f783bb9144b7f05d2ce04d4
Daniel Underwood <daniel.underwood13@gmail.com>
2014-11-01 18:08:13 -0400

    Finish basic texture class

--- include/texture.h
+++ include/texture.h
@@ -7,8 +7,11 @@
 class Texture
 {
 public:
-    Texture();
+    Texture(const char * filename, GLenum textureType = GL_TEXTURE_2D);
     ~Texture();
+
+    // Bind texture for use
+    void bind();
 
     // Getter for textureID
     GLuint getTextureID();
@@ -16,4 +19,11 @@ public:
 private:
     // OpenGL's ID for the texture
     GLuint textureID;
+
+    // Type identifier of texture
+    GLenum textureType;
+
+    // ID of Current Texture
+    // TODO: Make this a texture object
+    static GLuint currentTexture;
 };
--- src/texture.cpp
+++ src/texture.cpp
@@ -1,7 +1,45 @@
+#include <SOIL.h>
+#include <stdio.h>
 #include "texture.h"
+#include "../deps/glfw/deps/EGL/eglext.h"
 
-Texture::Texture() {}
+GLuint Texture::currentTexture = 0;
 
-Texture::~Texture() {}
+Texture::Texture(const char * filename, GLenum textureType) :
+    textureType(textureType)
+{
+    // Generate OpenGL texture
+    glGenTextures(1, &textureID);
+
+    // Load Image
+    // TODO: Error Handling
+    printf("Loading %s as texture %d\n", filename, textureID);
+    fflush(stdout);
+    int height, width;
+    unsigned char * texImage = SOIL_load_image(filename, &width, &height, 0, SOIL_LOAD_RGB);
+    printf("%s successfully loaded\n", filename);
+
+    // Assign Texture
+    // TODO: Make this work with 1D and 3D textures
+    glTexImage2D(textureType, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texImage);
+
+    // Free Image Data
+    SOIL_free_image_data(texImage);
+
+    // Generate Mipmaps
+    glGenerateMipmap(textureType);
+}
+
+Texture::~Texture()
+{
+    // Delete textures
+    glDeleteTextures(textureType, &textureID);
+}
+
+void Texture::bind()
+{
+    currentTexture = textureID;
+    glBindTexture(1, textureID);
+}
 
 GLuint Texture::getTextureID() { return textureID; }
