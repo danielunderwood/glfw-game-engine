df2a1949db114f8ef6153b4a99b724bb46382ade
Daniel Underwood <daniel.underwood13@gmail.com>
2014-11-15 19:22:05 -0500

    Use defines for defining unit vectors

--- include/geometry.h
+++ include/geometry.h
@@ -4,22 +4,11 @@
 
 namespace GGE
 {
-    // Enum for cartesian unit vectors
-    typedef enum
-    {
-        X_UNIT;
-        NEGATIVE_X_UNIT;
-
-        Y_UNIT;
-        NEGATIVE_Y_UNIT;
-
-        Z_UNIT;
-        NEGATIVE_Z_UNIT;
-
-        // Number of axes since first is defined by 0
-        NUM_AXES;
-    } AXIS;
-
-    // Get Vector of Axis
-    glm::vec3 getAxisVector(AXIS a);
+    // Unit vector defines
+    #define X_UNIT_VECTOR           glm::vec3(1.0, 0.0, 0.0)
+    #define NEGATIVE_X_UNIT_VECTOR  glm::vec3(-1.0, 0.0, 0.0)
+    #define Y_UNIT_VECTOR           glm::vec3(0.0, 1.0, 0.0)
+    #define NEGATIVE_Y_UNIT_VECTOR  glm::vec3(0.0, -1.0, 0.0)
+    #define Z_UNIT_VECTOR           glm::vec3(0.0, 0.0, 1.0)
+    #define NEGATIVE_Z_UNIT_VECTOR  glm::vec3(0.0, 0.0, -1.0)
 }
