18f3dbb663d552f4e7e02d29b05b7d9d9ffac90a
Daniel Underwood <daniel.underwood13@gmail.com>
2014-11-19 18:11:15 -0500

    Update entity with physics parameters

--- include/entity.h
+++ include/entity.h
@@ -21,19 +21,28 @@ namespace GGE
         // Getters
         glm::vec3 getPosition();
         glm::vec3 getDirection();
+        glm::vec3 getAcceleration();
+        glm::vec3 getVelocity();
+        glm::vec3 getAngularAcceleration();
+        glm::vec3 getAngularVelocity();
+
+        // Setters
+        virtual glm::vec3 setPosition(glm::vec3 newPosition);
+        virtual glm::vec3 setDirection(glm::vec3 newDirection);
+        virtual glm::vec3 setAcceleration(glm::vec3 newAcceleration);
+        virtual glm::vec3 setVelocity(glm::vec3 newVelocity);
+        virtual glm::vec3 setAngularAcceleration(glm::vec3 newAngularAcceleration);
+        virtual glm::vec3 setAngularVelocity(glm::vec3 newAngularVelocity);
 
         // Move entity's world position by vector
         virtual glm::vec3 move(glm::vec3 translation);
-
-        // Set entity's world position
-        virtual glm::vec3 setPosition(glm::vec3 newPosition);
 
         // Rotate entity
         // TODO: Change to work with quaternions
         virtual glm::vec3 rotate(float angle, glm::vec3 rotationAxis);
 
-        // Set entity's direction
-        virtual glm::vec3 setDirection(glm::vec3 newDirection);
+        // Update physics based on accelerations and velocities
+        virtual glm::vec3 updatePhysics(float deltaTime);
 
     protected:
         // TODO: Look at other structures for these, such as quaternions
@@ -41,5 +50,13 @@ namespace GGE
         glm::vec3 position;
         // Direction entity is facing
         glm::vec3 direction;
+        // Acceleration of entity
+        glm::vec3 acceleration;
+        // Velocity of entity
+        glm::vec3 velocity;
+        // Angular velocity (omega) of entity
+        glm::vec3 angularVelocity;
+        // Angular acceleration (alpha) of entity
+        glm::vec3 angularAcceleration;
     };
 }
--- src/entity.cpp
+++ src/entity.cpp
@@ -4,14 +4,24 @@
 
 namespace GGE
 {
-    Entity::Entity() {}
+    Entity::Entity() :
+        acceleration(glm::vec3(0.0, 0.0, 0.0)),
+        velocity(glm::vec3(0.0, 0.0, 0.0)),
+        angularAcceleration(glm::vec3(0.0, 0.0, 0.0)),
+        angularVelocity(glm::vec3(0.0, 0.0, 0.0))
+    {}
 
-    Entity::Entity(glm::vec3 position, glm::vec3 direction) : position(position), direction(direction) {}
+    Entity::Entity(glm::vec3 position, glm::vec3 direction) :
+            position(position), direction(direction) {}
 
     Entity::~Entity() {}
 
     glm::vec3 Entity::getPosition() { return position; }
     glm::vec3 Entity::getDirection() { return direction; }
+    glm::vec3 Entity::getAcceleration() { return acceleration; }
+    glm::vec3 Entity::getVelocity() { return velocity; }
+    glm::vec3 Entity::getAngularAcceleration() { return angularAcceleration; }
+    glm::vec3 Entity::getAngularVelocity() { return angularVelocity; }
 
     glm::vec3 Entity::move(glm::vec3 translation)
     {
@@ -22,26 +32,30 @@ namespace GGE
         return position;
     }
 
-    glm::vec3 Entity::setPosition(glm::vec3 newPosition)
-    {
-        // Set net position
-        position = newPosition;
-
-        // Return new position
-        return position;
-    }
-
     glm::vec3 Entity::rotate(float angle, glm::vec3 rotationAxis)
     {
         return direction = glm::rotate(direction, angle, rotationAxis);
     }
 
-    glm::vec3 Entity::setDirection(glm::vec3 newDirection)
+    glm::vec3 Entity::updatePhysics(float deltaTime)
     {
-        // Set direction of entity
-        direction = newDirection;
+        velocity += deltaTime * acceleration;
+        position += velocity;
 
-        // Return new direction of entity
-        return direction;
+        // TODO: Angular stuff
     }
+
+    glm::vec3 Entity::setPosition(glm::vec3 newPosition)
+    { return position = newPosition; }
+
+    glm::vec3 Entity::setDirection(glm::vec3 newDirection)
+    { return direction = newDirection; }
+    glm::vec3 Entity::setAcceleration(glm::vec3 newAcceleration)
+    { return acceleration = newAcceleration; }
+    glm::vec3 Entity::setVelocity(glm::vec3 newVelocity)
+    { return velocity = newVelocity; }
+    glm::vec3 Entity::setAngularAcceleration(glm::vec3 newAngularAcceleration)
+    { return angularAcceleration = newAngularAcceleration; }
+    glm::vec3 Entity::setAngularVelocity(glm::vec3 newAngularVelocity)
+    { return angularVelocity = newAngularVelocity; }
 }
