96dd9ad515a3d1a9f00cce7856fe7a15e8fcf8d6
Daniel Underwood <daniel.underwood13@gmail.com>
2014-11-21 15:03:05 -0500

    Add angle conversions to geometry.h

--- include/geometry.h
+++ include/geometry.h
@@ -11,4 +11,9 @@ namespace GGE
     #define NEGATIVE_Y_UNIT_VECTOR  glm::vec3(0.0, -1.0, 0.0)
     #define Z_UNIT_VECTOR           glm::vec3(0.0, 0.0, 1.0)
     #define NEGATIVE_Z_UNIT_VECTOR  glm::vec3(0.0, 0.0, -1.0)
+
+    // Degrees-Radians
+    #define PI 3.14159
+    #define DEGREES_TO_RADIANS(x) ((x) * PI / 180.0)
+    #define RADIANS_TO_DEGREES(x) ((x) * 180.0 / PI)
 }
